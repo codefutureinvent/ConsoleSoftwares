f361c660ee0541c952e4654856e881d8f6d78a9f
vmatos <vmatos@mwneo.com>
2021-06-06 01:11:42 -0300

    2021-06-06

--- WeatherGrabber/weather_grabber.py
+++ WeatherGrabber/weather_grabber.py
@@ -13,23 +13,25 @@ import sys
 import os
 import re
 
-# Global variables
-YELLOW = '\u001b[34m'
+# Global variables for ascii colors
+BLUE = '\u001b[34m'
 GREEN = '\u001b[32m'
 RED = '\u001b[31m'
 
-res = requests.get('https://google.com/search?q=weather')
-
-# Operating system terminal clearing command
+# Operating system validation
 unknown = '*'
 commands = {
     'Linux': 'clear',
     'Darwin': 'clear',
     'Windows': 'cls',
-    unknown: 'Sorry, we do not work with this OS.'
+    unknown: 'OS not found!'
 }
 
-# Operating System validation
+
+def clear():
+    os.system(commands[op_system])
+
+
 op_system = platform.system()
 try:
     commands[op_system]
@@ -39,31 +41,26 @@ except KeyError:
     sys.exit()
 
 
-def clear():
-    os.system(commands[op_system])
-
-
-clear()
-print(YELLOW, 'Welcome To The Weather Grabber Console App')
-year = time.ctime().split()[-1]
-month = time.ctime().split()[1]
-day = time.ctime().split()[2]
-
-# Saving page request
+# Getting and saving request
+res = requests.get('https://google.com/search?q=weather')
 f = open('webpage.txt', 'w')
 f.write(res.text)
 
 file_text = Path('webpage.txt').read_text()
 city_compiler = re.compile(r'weather for \w+')
 mo = city_compiler.search(file_text)
-
 temperature = re.findall(r'\d+°\w', file_text)
+
+clear()
+print(BLUE, 'Welcome To The Weather Grabber Console App')
+year = time.ctime().split()[-1]
+month = time.ctime().split()[1]
+day = time.ctime().split()[2]
 
 try:
     print(GREEN, f'{mo.group().title()} - {temperature[0]}')
 except AttributeError:
     print(RED, 'No city was found!')
-
 print(GREEN, '{}/{}/{}'.format(month, day, year))
 
 f.close()
